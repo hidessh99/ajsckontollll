c277fa7f0c59d31f6cbf26a2605abf5c51974682
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-11 18:21:15 +0700

    Update cektrgo.sh

--- xray/cektrgo.sh
+++ xray/cektrgo.sh
@@ -61,6 +61,6 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "------------------------------------";
-echo "Script Mod By ADAM SIJA"
+echo "Script Bagoes Vpn"
 rm -rf /tmp/other.txt
 
