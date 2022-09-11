509f598f430767a15803dd3298d1e7603b7d9999
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-11 18:24:22 +0700

    Update addtrgo.sh

--- xray/addtrgo.sh
+++ xray/addtrgo.sh
@@ -67,7 +67,7 @@ echo -e "========================="
 echo -e "Link TrGo  		: ${link}" | tee -a /etc/log-create-user.log
 echo -e "Link TrGo (v2rayNG)	: ${link1}" | tee -a /etc/log-create-user.log
 echo -e "========================="
-echo -e "Script Mod By ADAM SIJA"
+echo -e "Script Bagoes Vpn"
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
 
