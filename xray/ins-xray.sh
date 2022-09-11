faa1075f3d3919f897752e6bd8b0f667ef12c794
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-11 20:38:58 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -613,7 +613,7 @@ wget -O cek-ws "https://raw.githubusercontent.com/sibeesans/ajsc/main/xray/cek-w
 
 # vless
 wget -O add-vless "https://raw.githubusercontent.com/sibeesans/ajsc/main/xray/add-vless.sh" && chmod +x add-vless
-wget -O trialvless "https://raw.githubusercontent.com/sibeesans/ajscmain/xray/trialvmess.sh" && chmod +x trialvless
+wget -O trialvless "https://raw.githubusercontent.com/sibeesans/ajsc/main/xray/trialvless.sh" && chmod +x trialvless
 wget -O renew-vless "https://raw.githubusercontent.com/sibeesans/ajsc/main/xray/renew-vless.sh" && chmod +x renew-vless
 wget -O del-vless "https://raw.githubusercontent.com/adasibeesans/ajsc/main/xray/del-vless.sh" && chmod +x del-vless
 wget -O cek-vless "https://raw.githubusercontent.com/sibeesans/ajsc/main/xray/cek-vless.sh" && chmod +x cek-vless
