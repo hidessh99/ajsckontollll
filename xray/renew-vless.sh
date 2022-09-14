d96d0b6dd71968f13557340d79ca4d8f3ccafdf0
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-14 13:12:06 +0700

    Update renew-vless.sh

--- xray/renew-vless.sh
+++ xray/renew-vless.sh
@@ -46,7 +46,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
     echo ""
   	grep -E "^#& " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
-    red "tap enter to go back"
+    read "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     if [ -z $user ]; then
