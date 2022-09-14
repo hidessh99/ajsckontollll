dc2d2e4c52db92c051753959db46450ed094d501
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-14 22:02:00 +0700

    Update proxy3.js

--- ssh/proxy3.js
+++ ssh/proxy3.js
@@ -56,7 +56,7 @@ const server = net.createServer();
 server.on('connection', function(socket) {
     var packetCount = 0;
     //var handshakeMade = false;
-    socket.write("HTTP/1.1 101 Autoscript By OTTIN NETWORK\r\nContent-Length: 1048576000000\r\n\r\n", function(err) {
+    socket.write("HTTP/1.1 101 Autoscript By Bagoes Vpn\r\nContent-Length: 1048576000000\r\n\r\n", function(err) {
         if(err) {
             console.log("[SWRITE] Failed to write response to " + socket.remoteAddress + ":" + socket.remotePort + ", error: " + err);
         }
