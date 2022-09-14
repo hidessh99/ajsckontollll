874099a96be1e8244e176e2fd1c19efa7d1e4882
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-14 16:24:58 +0700

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/sibeesans/ajsc/main/anjay/allow > /root/tmp
+    curl -sS https://raw.githubusercontent.com/sibeesans/ajsc/main/anjay/allow> /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
