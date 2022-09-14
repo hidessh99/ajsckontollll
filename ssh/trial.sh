080da24e335c811c4c1c01f3ffc9fa0a5418b4cd
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-14 16:59:57 +0700

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -166,7 +166,7 @@ echo -e "
 GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]
 "
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-fi
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
-trial-menu
+menu
+fi
