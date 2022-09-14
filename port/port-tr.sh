4aff02b40a904147f282f1e62dd65d77322fa1a6
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-14 19:24:16 +0700

    Update port-tr.sh

--- port/port-tr.sh
+++ port/port-tr.sh
@@ -2,12 +2,6 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
 clear
 tr="$(cat ~/log-install.txt | grep -i "Trojan-GFW" | cut -d: -f2|sed 's/ //g')"
 echo -e "Name : Change Port Trojan-GFW"
