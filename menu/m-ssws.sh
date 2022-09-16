6a7dfd9ff95459f911439ba1341332543ecafaf5
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-16 13:51:46 +0700

    Update and rename m-trgo.sh to m-ssws.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -21,14 +21,14 @@ exit 0
 fi
 clear 
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      • TROJAN GO MENU •          \E[0m"
+echo -e "\E[0;100;33m      • SS WS/GRPC •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Create Account Trojan Go "
-echo -e " [\e[36m•2\e[0m] Trial Account Trojan Go "
-echo -e " [\e[36m•3\e[0m] Extending Account Trojan Go Active Life "
-echo -e " [\e[36m•4\e[0m] Delete Account Trojan Go "
-echo -e " [\e[36m•5\e[0m] Check User Login Trojan Go"
+echo -e " [\e[36m•1\e[0m] Create Account SS WS/GRPC "
+echo -e " [\e[36m•2\e[0m] Trial Account SS WS/GRPC "
+echo -e " [\e[36m•3\e[0m] Extending Account SS WS/GRPC Active Life "
+echo -e " [\e[36m•4\e[0m] Delete Account SS WS/GRPC "
+echo -e " [\e[36m•5\e[0m] Check User Login SS WS/GRPC"
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
@@ -39,10 +39,10 @@ echo -e ""
 read -p " Select menu : " opt
 echo -e ""
 case $opt in
-1) clear ; addtrgo ;;
+1) clear ; addssws.sh ;;
 2) clear ; trialtrojango ;;
-3) clear ; renewtrgo ;;
-4) clear ; deltrgo ;;
+3) clear ; delssws.sh ;;
+4) clear ; cekssws.sh;;
 5) clear ; cektrgo ;;
 0) clear ; menu ;;
 x) exit ;;
