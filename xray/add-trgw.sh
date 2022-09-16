a64f91f559e88e39c1a315efedc80ba78c1476bb
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-16 20:43:43 +0700

    Update add-trgw.sh

--- xray/add-trgw.sh
+++ xray/add-trgw.sh
@@ -1,5 +1,34 @@
-#!/bin/bash
-domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
-chown www-data.www-data $domainSock_dir
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do                read -rp "ชื่อ: " -e user
+                CLIENT_EXISTS=$(grep -w $user /etc/v2ray/trojan.json | wc -l)
 
-systemctl restart xray
+                if [[ ${CLIENT_EXISTS} == '1' ]]; then
+                        echo ""
+                        echo " มีชื่อในระบบแล้ว โปรดเลือกชื่ออื่น."
+                        exit 1
+                fi
+        done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "วันหมดอายุ (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"password": "'""$user""'","email": "'""$user""'"' /etc/v2ray/trojan.json
+systemctl restart v2ray@trojan
+trojanlink="trojan://${user}@${domain}:443"
+clear
+echo -e ""
+echo -e "********************************" 
+echo -e "           ข้อมูลบัญชี"   
+echo -e "ชื่อ          : ${user}"
+echo -e "โฮสต์        : ${domain}"
+echo -e "พอร์ต        : 443"
+echo -e "คีย์          : ${user}"
+echo -e "วันหมดอายุ    : $exp"
+echo -e "ลิงก์         : ${trojanlink}"
+echo -e "*********************************"
+echo -e "สคริปโดยเอเน้ต"
