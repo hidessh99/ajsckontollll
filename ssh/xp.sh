e911ea4ff448c13d08e1c79f85febde169823226
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-16 14:07:47 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -110,22 +110,6 @@ fi
 done
 systemctl restart xray
 
-#----- Auto Remove Trojan Go
-data=( `cat /etc/trojan-go/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^#! $user" "/etc/trojan-go/config.json" | cut -d ' ' -f 3 | sort | uniq)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" -le "0" ]]; then
-sed -i "/^#! $user $exp/,/^},{/d" /etc/trojan-go/config.json
-sed -i "/^#! $user $exp/,/^},{/d" /etc/trojan-go/config.json
-fi
-done
-systemctl restart trojan-go.service
-
 
 ##------ Auto Remove SSH
 hariini=`date +%d-%m-%Y`
