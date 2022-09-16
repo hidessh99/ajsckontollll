8d1e83f0b712a26e22c2696eb2bf44f621e5593a
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-16 21:12:18 +0700

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -25,10 +25,11 @@ echo -e "\E[0;100;33m      • TROJAN GRPC MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create Account Trojan "
-echo -e " [\e[36m•2\e[0m] Trial Account Trojan "
-echo -e " [\e[36m•3\e[0m] Extending Account Trojan Active Life "
-echo -e " [\e[36m•4\e[0m] Delete Account Trojan "
-echo -e " [\e[36m•5\e[0m] Check User Login Trojan "
+echo -e " [\e[36m•2\e[0m] Create Account Trojan GFW "
+echo -e " [\e[36m•3\e[0m] Trial Account Trojan "
+echo -e " [\e[36m•4\e[0m] Extending Account Trojan Active Life "
+echo -e " [\e[36m•5\e[0m] Delete Account Trojan "
+echo -e " [\e[36m•6\e[0m] Check User Login Trojan "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
@@ -40,10 +41,11 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-tr ;;
-2) clear ; trialtrojan ;;
-3) clear ; renew-tr ;;
-4) clear ; del-tr ;;
-5) clear ; cek-tr ;;
+2) clear ; add-trgf ;;
+3) clear ; trialtrojan ;;
+4) clear ; renew-tr ;;
+5) clear ; del-tr ;;
+6) clear ; cek-tr ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo "SALAH TEKAN" ; sleep 1 ; m-trojan ;;
