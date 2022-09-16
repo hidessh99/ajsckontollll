245fdfc532f4885f6362c0dbed3f9eb7917b130e
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-16 16:20:53 +0700

    Update renew-ssws.sh

--- xray/renew-ssws.sh
+++ xray/renew-ssws.sh
@@ -35,7 +35,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo ""
         read -n 1 -s -r -p "Press any key to back on menu"
-        v2ray-menu
+        menu
 	fi
 
 	clear
@@ -45,7 +45,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo ""
   	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
-    red "tap enter to go back"
+    read "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     if [ -z $user ]; then
