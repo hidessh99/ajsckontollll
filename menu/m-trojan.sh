5f18d8e0cc73eb0927bfe7e080b46b34635dca23
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-17 15:55:57 +0700

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -41,7 +41,7 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-tr ;;
-2) clear ; add-trgw ;;
+2) clear ; add-trgfw ;;
 3) clear ; trialtrojan ;;
 4) clear ; renew-tr ;;
 5) clear ; del-tr ;;
